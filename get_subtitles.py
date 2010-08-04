558b164a0bcccd0b28f15aed47b6e6abcb75122c
Pedro Araújo <pedromcaraujo@gmail.com>
2010-08-04 17:57:01 +0100

    Finished get_subtitles script. It downloads, renames and places the subtitles with the videos

--- get_subtitles.py
+++ get_subtitles.py
@@ -1,4 +1,4 @@
-import struct, os, sys, subprocess
+import struct, os, sys, subprocess, urllib
 from xmlrpclib import ServerProxy,ProtocolError
 
 MOVIE_FORMATS = ['avi','mkv']
@@ -50,12 +50,45 @@ except ProtocolError:
     sys.exit()
 
 # [{'moviebytesize': '366786266', 'sublanguageid': 'eng', 'moviehash': 'dcb7dbae558c63f6'}, {'moviebytesize': '576700632', 'sublanguageid': 'eng', 'moviehash': 'f2671cfa1d9ece6f'}]
-
+videos_info = []
 query = []
 for video in videos:
     returnedhash,filesize = getMovieHash(video)
+    videos_info.append({'file':video,'moviehash':returnedhash,'sublanguageid':'eng','moviebytesize':str(filesize)})
     query.append({'moviehash':returnedhash,'sublanguageid':'eng','moviebytesize':str(filesize)})
 
 print query
 
-print s.SearchSubtitles(res['token'],query)
+subtitles = s.SearchSubtitles(res['token'],query)
+
+for i in subtitles['data']:
+    movie_hash = i['MovieHash']
+    for h in videos_info:
+        if movie_hash == h['moviehash'] and h.get('subdownloadlink',None) is None:
+            h.update({'subdownloadlink':i['SubDownloadLink'],'subformat':i['SubFormat']})
+
+print videos_info
+
+
+#let download the subtitles, unpack them, change the name and put them together with their respective file
+
+for video in videos_info:
+    """ """
+    filename = video['file'].split('/')[-1]
+    path = '/'.join(video['file'].split('/')[:-1])
+    for format in MOVIE_FORMATS:
+        filename = filename.rstrip(format)
+    url = video['subdownloadlink']
+    webFile = urllib.urlopen(url)
+    subtitle_file = "%s/%s"%(path,url.split('/')[-1])
+    subtitle_filename = url.split('/')[-1].rstrip('.gz')
+    localFile = open(subtitle_file, 'w')
+    localFile.write(webFile.read())
+    webFile.close()
+    localFile.close()
+    subprocess.Popen(['gzip','-d',subtitle_file]).wait()
+    os.rename('%s/%s'%(path,subtitle_filename),'%s/%s%s'%(path,filename,video['subformat']))
+
+
+
+
